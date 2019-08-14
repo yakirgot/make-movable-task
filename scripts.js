989b9de698d5413c63aa223d5ae6629f355eb6f9
yakirgot <got.yakir@gmail.com>
2019-08-14 23:43:00 +0300

    cut down on variables

--- scripts.js
+++ scripts.js
@@ -1,50 +1,55 @@
-function makeMoveable(element) {
-  element.addEventListener('mousedown', (mouseDownEvent) => {
-    if (mouseDownEvent.buttons !== 1) {
-      return;
-    }
+(() => {
+  function makeMoveable(element) {
+    element.addEventListener('mousedown', (mouseDownEvent) => {
+      if (mouseDownEvent.buttons !== 1) {
+        return;
+      }
 
-    const elementCoordinates = element.getBoundingClientRect();
-    const translateValues = getTranslateValues(element);
+      const elementCoordinates = element.getBoundingClientRect();
+      const translateValues = getTranslateValues(element);
 
-    const leftGapInElement = mouseDownEvent.clientX - elementCoordinates.left;
-    const topGapInElement = mouseDownEvent.clientY - elementCoordinates.top;
+      const leftGapInElement = mouseDownEvent.clientX - elementCoordinates.left;
+      const topGapInElement = mouseDownEvent.clientY - elementCoordinates.top;
 
-    document.onmousemove = (mouseMoveEvent) => {
-      requestAnimationFrame(() => {
-        let xCoordinate =
-          mouseMoveEvent.clientX - elementCoordinates.left - leftGapInElement;
-        let yCoordinate =
-          mouseMoveEvent.clientY - elementCoordinates.top - topGapInElement;
+      document.onmousemove = (mouseMoveEvent) => {
+        requestAnimationFrame(() => {
+          const coordinates = { x: 0, y: 0 };
 
-        if (translateValues) {
-          xCoordinate += translateValues.xTranslate;
-          yCoordinate += translateValues.yTranslate;
-        }
+          coordinates.x =
+            mouseMoveEvent.clientX - elementCoordinates.left - leftGapInElement;
+          coordinates.y =
+            mouseMoveEvent.clientY - elementCoordinates.top - topGapInElement;
 
-        element.style.transform = `translate(${xCoordinate}px, ${yCoordinate}px)`;
-      });
-    };
+          if (translateValues) {
+            coordinates.x += translateValues.x;
+            coordinates.y += translateValues.y;
+          }
 
-    document.onmouseup = () => {
-      document.onmousemove = null;
-      document.onmouseup = null;
-    };
-  });
-}
+          element.style.transform = `translate(${coordinates.x}px, ${coordinates.y}px)`;
+        });
+      };
 
-function getTranslateValues(element) {
-  const transformValue = getComputedStyle(element).getPropertyValue(
-    'transform',
-  );
-
-  if (transformValue !== 'none') {
-    const matrixArray = transformValue.split(', ');
-    const xTranslate = Number(matrixArray[4]);
-    const yTranslate = Number(matrixArray[5].slice(0, -1));
-
-    return { xTranslate, yTranslate };
+      document.onmouseup = () => {
+        document.onmousemove = null;
+        document.onmouseup = null;
+      };
+    });
   }
-}
 
-document.querySelectorAll('li').forEach(makeMoveable);
+  function getTranslateValues(element) {
+    const transformValue = getComputedStyle(element).getPropertyValue(
+      'transform',
+    );
+
+    if (transformValue !== 'none') {
+      const matrixArray = transformValue.split(', ');
+
+      const xTranslate = Number(matrixArray[4]);
+      const yTranslate = Number(matrixArray[5].slice(0, -1));
+
+      return { x: xTranslate, y: yTranslate };
+    }
+  }
+
+  document.querySelectorAll('li').forEach(makeMoveable);
+})();
